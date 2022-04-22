0be339522fb987eeee17103cd0cfa7097468b7e7
unknown <den.zv95@yandex.ru>
2022-04-22 18:26:04 +0900

    test deploy

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -8,6 +8,7 @@ module.exports = gql`
     checked: Boolean
     deadline: String
   }
+
   type User {
     _id: ID!
     username: String
@@ -15,29 +16,35 @@ module.exports = gql`
     password: String
     token: String
   }
+
   input TaskInput {
     text: String
     createdBy: String
     checked: Boolean
     deadline: String
   }
+
   input RegisterInput {
     username: String
     email: String
     password: String
   }
+
   input LoginInput {
     email: String
     password: String
   }
+
   input TaskWhereInput {
     createdBy: String
   }
+
   type Query {
     task(id: ID!): Task
     findManyTask(where: TaskWhereInput): [Task!]
     user(id: ID!): User
   }
+
   type Mutation {
     createTask(taskInput: TaskInput): Task!
     registerUser(registerInput: RegisterInput): User
